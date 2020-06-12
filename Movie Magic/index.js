ce9202f3e0b5195df14ea501e756c46a3a076bc6
Jitesh <jiteshanand7@gmail.com>
2020-06-12 12:37:33 +0530

    Code change.

--- Movie Magic/index.js
+++ Movie Magic/index.js
@@ -1,22 +1,103 @@
-const fetchData = async (searchTerm) => {
+const autoCompleteConfig = {
+    renderOption(movie) {
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        return `
+          <img src="${imgSrc}" />
+          ${movie.Title} (${movie.Year})
+        `;
+    },
+    inputValue(movie){
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/',{
+            params:{
+                apikey:'d9835cc5',
+                s: searchTerm
+            }
+        });
+        if(response.data.Error){
+            return [];
+        }
+        return response.data.Search;
+    }
+};
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root:document.querySelector("#left-autocomplete"),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector("#left-summary"));
+    }
+});
+
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root:document.querySelector("#right-autocomplete"),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector("#right-summary"));
+    }
+});
+
+const movieTemplate = (movieDetail) =>{
+    console.log(movieDetail);
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">
+                ${movieDetail.Awards}
+            </p>
+            <p class"subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">
+                ${movieDetail.BoxOffice}
+            </p>
+            <p class"subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">
+                ${movieDetail.Metascore}
+            </p>
+            <p class"subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">
+                ${movieDetail.imdbRating}
+            </p>
+            <p class"subtitle">IMDB Ratings</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">
+                ${movieDetail.imdbVotes}
+            </p>
+            <p class"subtitle">IMDB Votes</p>
+        </article>
+    `
+ s  }
+
+const onMovieSelect = async (movie,summaryTarget) =>{
     const response = await axios.get('http://www.omdbapi.com/',{
         params:{
             apikey:'d9835cc5',
-            s: searchTerm
+            i: movie.imdbID
         }
     });
-    console.log(response.data);
-};
-
-let timeoutId;
-const onInput = (event) => {
-    if(timeoutId){
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => 
-    {
-        fetchData(event.target.value)
-    },1000);
+    summaryTarget.innerHTML = movieTemplate(response.data);
 }
-
-document.getElementById("searchMovie").addEventListener('input',onInput);
